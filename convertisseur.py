933b049d229eae09cabcef4a35b33ac85ace52d4
guada-s <167087535+guada-s@users.noreply.github.com>
2024-04-15 14:24:05 +0200

    Add files via upload

---
+++ convertisseur.py
@@ -0,0 +1,38 @@
+answer = input ("Quelle unitée souhaitez\n vous convertir ?" )
+
+if answer == "fahrenheit":
+    f = input ("fahrhenheit:" )
+    c = ( int(f) - 32) * 5//9
+    print ( str(c) + "c°" )
+else:
+    c = input ("celsius:")
+    f = ( int(c) * 9/5) + 32
+    print ( str(f) + "F°")
+    
+
+"""
+f = input("fahrenheit:")
+c = ( int(f) - 32) * 5//9
+print ( str(c) + "c°")
+"""
+
+"""
+t = input("tasse:")
+ml = int(t) * 250
+print ( str(ml) + "ml" )
+"""
+
+"""
+h = input ("combien d'heures écoulées?")
+m = input ("combien de minutes écoulées?")
+s = input ("combien de  secondes écoulées?")
+secondes = (int(h) * 3600 ) + ( int(m) * 60 ) + int(s)
+input ( str(secondes) + "s se sont écoulées")
+"""
+"""
+secondes = input("combien de secondes se sont écoulées?")
+heures = int(secondes)//3600
+minutes =  (int(secondes) - 3600 ) //60
+sec = ( int(secondes) - 3600) - ( int(minutes) * 60)
+print ( str(heures) + "heures," + str(minutes) + "minutes et " + str(sec) + "secondes" )
+"""
