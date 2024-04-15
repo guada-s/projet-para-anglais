cd014856319250a0957c6b803d735d03c4c872ba
guada-s <167087535+guada-s@users.noreply.github.com>
2024-04-15 19:25:55 +0200

    Update convertisseur.py

--- convertisseur.py
+++ convertisseur.py
@@ -1,15 +1,20 @@
-answer = input ("Quelle unitée souhaitez\n vous convertir ?" )
-
-if answer == "fahrenheit":
-    f = input ("fahrhenheit:" )
-    c = ( int(f) - 32) * 5//9
-    print ( str(c) + "c°" )
-else:
-    c = input ("celsius:")
-    f = ( int(c) * 9/5) + 32
-    print ( str(f) + "F°")
+while ' ':
+    print("Repondez par A ou par B")
+    answer = input ("Quelle unitée souhaitez vous convertir ?" )
+    A= "fahrenheit"
+    B = "celsius"
     
-
+    if answer == "A":
+        f = input ("fahrhenheit:" )
+        c = ( int(f) - 32) * 5//9
+        print ( str(c) + "c°" )
+        break
+    
+    elif answer == "B":
+            c = input ("celsius:")
+            f = ( int(c) * 9/5) + 32
+            print ( str(f) + "F°")
+            break
 """
 f = input("fahrenheit:")
 c = ( int(f) - 32) * 5//9
